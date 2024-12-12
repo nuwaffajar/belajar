444e535ef48c6ea595354e84dbe0a47667bf818a
nuwaffajar <nuwaffajar@gmail.com>
2024-12-12 18:54:20 +0700

    pertama

---
+++ resources/views/kontak.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h1>Ini Kontak</h1>
+</x-layout>
---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home', ['title' => 'Home']);
+});
+
+Route::get('/about', function () {
+    return view('about', ['title' => 'About']);
+});
+
+Route::get('/blog', function () {
+    return view('blog', ['title' => 'Blog']);
+});
+
+Route::get('/kontak', function () {
+    return view('kontak', ['title' => 'Kontak']);
+});
